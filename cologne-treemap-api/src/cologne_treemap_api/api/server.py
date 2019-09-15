f317b176ef2a5e0bc1ae7160c0f11fabd05f6759
zushicat <karinhoehne@akasushi.de>
2019-09-15 14:39:51 +0200

    error messages: server

--- cologne-treemap-api/src/cologne_treemap_api/api/server.py
+++ cologne-treemap-api/src/cologne_treemap_api/api/server.py
@@ -11,9 +11,8 @@ from cologne_treemap_api.treemap import geo_numbers_by_district_number
 
 
 def http_exception(req: Request, e: HTTPException) -> Response:
-    # This is only somewhat jsonrpc: no id, no notion of batch ... still return something meaningful.
-    return Response(json.dumps({'error': {'code': e.code, 'message': e.description}}).encode(),
-                    content_type='application/json', status=e.code)
+    return Response(json.dumps(
+        {"status": f"ERROR: {e.description}", }).encode(), content_type='application/json', status=e.code)
 
 
 def json_rpc_except(func: Callable) -> Callable:
@@ -23,7 +22,7 @@ def json_rpc_except(func: Callable) -> Callable:
         except JSONRPCDispatchException:
            raise
         except Exception as e:
-           raise InternalError(f'Something unexpected happened! {e}')
+           raise InternalError(f'INTERNAL ERROR: {e}')
     return func_wrapper
 
 
@@ -36,7 +35,6 @@ def application(request):
         return (Response(response.json, mimetype='application/json'))
     except Exception as e:
         return Response(json.dumps(
-            {'error': {'code': 1, 'message': f'Something unexpected happened! Please check your json request. {e}'}}).encode(),
-            content_type='application/json')
+            {"status": f"ERROR: {e}"}).encode(), content_type='application/json')
     except HTTPException as e:
         return http_exception(request, e)
