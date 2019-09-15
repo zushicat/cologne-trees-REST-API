13307ad487859939da9fb37ed4c47b16131198d0
zushicat <karinhoehne@akasushi.de>
2019-09-15 17:49:46 +0200

    cleanup

--- cologne-treemap-api/src/cologne_treemap_api/treemap/my_application.py
+++ cologne-treemap-api/src/cologne_treemap_api/treemap/my_application.py
@@ -2,7 +2,6 @@ import json
 import os
 from typing import Any, Dict, List
 
-from ._make_fancy_shit import do_something
 from ._by_geo import get_geo_numbers_by_district_number, get_geo_numbers_by_suburb_number, get_geo_genus_numbers_by_suburb_number
 
 DIRNAME = os.environ["DATA_LOCATION"]
