4a3d4fd33b9003a74a8e5877020d9926c1fb9157
zushicat <karinhoehne@akasushi.de>
2019-09-18 12:41:12 +0200

    more endpoints

--- cologne-treemap-api/src/cologne_treemap_api/treemap/__init__.py
+++ cologne-treemap-api/src/cologne_treemap_api/treemap/__init__.py
@@ -3,9 +3,11 @@ from .my_application import (
     geo_numbers_by_district_number,
     geo_numbers_by_suburb_number,
     geo_genus_numbers_by_suburb_number,
+    geo_age_by_suburb_number,
     tree_attr_age_numbers,
     tree_attr_genus_numbers,
-    geo_age_by_suburb_number,
+    tree_attr_name_german_numbers,
+    tree_geo_name_german_suburb_numbers,
 )
 
 __all__ = [
@@ -16,4 +18,6 @@ __all__ = [
     "load_tree_data",
     "tree_attr_age_numbers",
     "tree_attr_genus_numbers,",
+    "tree_attr_name_german_numbers",
+    "tree_geo_name_german_suburb_numbers",
 ]
---
+++ cologne-treemap-api/src/cologne_treemap_api/treemap/_by_tree_geo.py
@@ -0,0 +1,9 @@
+import json
+import datetime
+
+from typing import Any, Dict, List
+
+def get_tree_geo_name_german_suburb_numbers(
+    treemap: List[Dict[str, Any]], suburb_number: str = None
+) -> Dict[str, Any]:
+    print(f"hello {suburb_number}")
