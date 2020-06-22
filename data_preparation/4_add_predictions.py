97c023c072501f8414b69cf201f791b1eb71ac57
zushicat <karinhoehne@akasushi.de>
2020-06-22 13:47:46 +0200

    optimize id match

--- data_preparation/4_add_predictions.py
+++ data_preparation/4_add_predictions.py
@@ -16,13 +16,28 @@ def _get_compressed_tree_data() -> List[str]:
     return lines
 
 
+def _get_predictions_age() -> Dict[str, Dict[str, Any]]:
+    '''
+    columns: tree_id, age_group_2020, probabiliy
+    '''
+    df_predictions_age = pd.read_csv("data_prediction/data/predictions_age_group.csv")
+
+    predictions_age: Dict[str, Dict[str, Any]] = {}
+    for index, row in df_predictions_age.iterrows():
+        predictions_age[row["tree_id"]] = {
+            "age_group": row["age_group_2020"],
+            "probability": row["probabiliy"]
+        }
+    
+    return predictions_age
+
 
 if __name__ == "__main__":
     tree_data_str = _get_compressed_tree_data()
-    df_predictions_age = pd.read_csv("data_prediction/data/predictions_age_group.csv")
+    predictions_age = _get_predictions_age()
     
     lines: List[Dict[str, Any]] = []
-    for line in tree_data_str:
+    for i, line in enumerate(tree_data_str):
         try:
             tree_data = json.loads(line)
         except:
@@ -32,23 +47,23 @@ if __name__ == "__main__":
             tree_data["predictions"]: Optional[Dict[str, Any]] = None
 
         tree_id = tree_data["tree_id"]
-        tree_prediction_list = df_predictions_age.loc[df_predictions_age['tree_id'] == tree_id]
         
-        if len(tree_prediction_list) == 0:
-            lines.append(tree_data)
+        if predictions_age.get(tree_id) is None:
             continue
 
-        current_prediction = tree_prediction_list.iloc[0]
+        current_prediction = predictions_age[tree_id]
         
         if tree_data.get("predictions") is None:
             tree_data["predictions"] = {}
+
         tree_data["predictions"]["age_prediction"] = {
-            "age_group_2020": int(current_prediction["age_group_2020"]),
-            "probabiliy_age_group_2020": float(current_prediction["probabiliy"])
+            "age_group_2020": int(current_prediction["age_group"]),
+            "probabiliy_age_group_2020": float(current_prediction["probability"])
         }
 
         lines.append(tree_data)
 
+    
     # write cleaned file
     with open("trees_cologne_merged.jsonl", "w") as f:  # overwrite old data
         for line in lines:
